9ee27acf80dcd32a24b0be07d26fff7f835bf443
Siddhant0122 <siddhant.chavan126@gmail.com>
2026-04-28 01:35:29 +0530

    Converting a string to Ascii

---
+++ AsciiToByteArray.js
@@ -0,0 +1,8 @@
+function asciiToBytes(asciiString){
+    return new Uint8Array([...asciiString.map(char => char.charCodeAt(0))]);
+}
+
+//example usage
+const asciiString = "Hello";
+const byteArray = asciiToBytes(asciiString);
+console.log(byteArray);
