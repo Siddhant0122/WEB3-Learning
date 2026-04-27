f726b8e0109d533a78ca9b9a99249e28da1893bd
Siddhant0122 <siddhant.chavan126@gmail.com>
2026-04-28 01:29:23 +0530

    Encoding using an Uint8Array

---
+++ UInt8ArrayToAscii.js
@@ -0,0 +1,7 @@
+function bytesToAscii(byteArray) {
+    return new TextDecoder().decode(byteArray);
+}
+//example usage
+const bytes = new Uint8Array([72, 101, 108, 108, 111]);
+const asciiString = bytesToAscii(bytes);
+console.log(asciiString);
