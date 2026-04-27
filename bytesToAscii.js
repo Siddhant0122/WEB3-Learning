540b68cd8d27dcc9e2361a76a00cb05713d5086c
Siddhant0122 <siddhant.chavan126@gmail.com>
2026-04-28 01:24:01 +0530

    Encoding

---
+++ bytesToAscii.js
@@ -0,0 +1,8 @@
+function bytesToAscii(byteArray){
+    return byteArray.map(byte => String.fromCharCode(byte)).join('');
+}
+
+//Example usage
+const bytes = [72, 101, 108, 108, 111];
+const asciiString = bytesToAscii(bytes);
+console.log(asciiString);
